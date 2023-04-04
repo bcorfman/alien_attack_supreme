f62088e0693a0f089073d4308e2695a6abb5c2d0
Brandon Corfman <bcorfman@fastmail.fm>
2023-04-03 22:54:56 -0400

    Explosion fixed

--- characters/enemy.gd
+++ characters/enemy.gd
@@ -1,59 +1,54 @@
 extends RigidBody2D
 
-
-var Explosion = preload("res://characters/explosion.tscn")
+signal explosions_finished
 var explosions = []
-var loc
-var sz
+var explosion_sounds = []
+@onready var random = RandomNumberGenerator.new()
 
 
-func _provide_jitter():
-	var random = RandomNumberGenerator.new()
-	random.randomize()
-	var jitter_x = random.randfn(loc.x, int(sz / 5.0))
-	var jitter_y = random.randfn(loc.y, int(sz / 5.0))
+func _get_explosion(idx: int) -> AnimatedSprite2D:
+	return $Ship.get_node("Explosion" + str(idx))
+
+func _get_explosion_sound(idx: int) -> AudioStreamPlayer2D:
+	return get_node("ExplosionSound" + str(idx))
+
+	
+func _provide_jitter(extent: float):
+	var rng = extent / 3.0
+	var jitter_x = random.randf_range(-rng, rng)
+	var jitter_y = random.randf_range(-rng, rng)
 	return Vector2(jitter_x, jitter_y)
 	
+func animation_finished():
+	var unfinished_explosions = false
+	for i in [1, 5]:
+		var explosion = _get_explosion(i)
+		if not explosion.is_playing():
+			explosion.visible = false
+			explosion.hide()
+			var sound: AudioStreamPlayer2D = _get_explosion_sound(i)
+			sound.play()
+		if explosion.visible:
+			unfinished_explosions = true
+			
+	if not unfinished_explosions:		
+		explosions_finished.emit()
+
 func explode():
-	loc = _provide_jitter()
-	var explosion: AnimatedSprite2D = Explosion.instantiate()
-	get_parent().add_child(explosion)
-	explosions.append(explosion)
-	explosion.position = loc
-	explosion.visible = true
+	var extent = $Ship.get_rect().size.x
+	var loc = _provide_jitter(extent)
+	var explosion = explosions.pop_back()
+	explosion.offset = loc
+	explosion.show()
 	explosion.connect("animation_finished", animation_finished)
 	explosion.play("default")
 	
-func animation_finished():
-	var exploded = []
-	for item in explosions:
-		if not item.is_playing():
-			exploded.append(item)
-	
-	for exp in exploded:
-		delete_explosion(exp)
-
-
-func delete_explosion(exp):
-	var index = explosions.find(exp)
-	if index > -1:
-		exp.hide()
-		exp.queue_free()
-		explosions.remove_at(index)
-		
-func die():
-	hide()
-	#queue_free()
-	
-func start_explosions(src_center: Vector2, src_size: int):
-	sz = src_size
-	loc = src_center
-	var random = RandomNumberGenerator.new()
+func start_explosions():
 	random.randomize()
-	for _i in range(random.randi_range(2, 4)):
+	for i in range(1, 5): 
+		var	explosion: AnimatedSprite2D = _get_explosion(i)
+		explosions.append(explosion)
 		var wait_time = random.randf_range(0.1, 0.4)
 		var timer: SceneTreeTimer = get_tree().create_timer(wait_time)
 		timer.connect("timeout", explode)
-	var timer: SceneTreeTimer = get_tree().create_timer(0.01)
-	timer.connect("timeout", die)
 	
